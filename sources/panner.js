bc9ac83079e21d4a07461e7c359db4d8df10e9bb
Rich Caloggero <rjc@mit.edu>
2016-08-24 23:39:20 -0400

    * created 3d Panner
    - doesn't work yet, but it renders
    - ried to use js class, but couldn't find a way to handle the Tuna style delegation (I think Tuna uses the delegation pattern as discussed in "You Don't Know js").

---
+++ sources/panner.js
@@ -0,0 +1,87 @@
+var epselon = 2 * Number.EPSILON;
+
+Tuna.prototype.Panner = function Panner () {
+var panner, listener, position = [0,0,0];
+var audio = Tuna.Super.context;
+this.input = audio.createGain ();
+this.output = audio.createGain ();
+this.activateNode = audio.createGain();
+this.inputGain = audio.createGain();
+this.outputGain = audio.createGain ();
+
+this.input.connect (this.inputGain);
+this.outputGain.connect (this.output);
+
+
+
+// public methods
+this.position = setPosition.bind (this);
+this.getPosition = getPosition.bind (this);
+
+init.bind (this)();
+console.log ("Panner initialized");
+
+
+// private methods
+
+function getPosition () {return position;}
+
+function setPosition (name, value) {
+if (distance (position,  value) < epselon) return false;
+
+position = value.slice();
+console.log (`setPosition: ${position}`);
+return true;
+} // setPosition
+
+
+function init () {
+panner = audio.createPanner ();
+this.inputGain.connect (panner).connect (this.outputGain);
+setPosition ("position", [0,0,0]);
+
+} // init
+
+function distance (p1, p2) {
+return Math.sqrt (
+Math.pow (p2[0]-p1[0], 2)
++ Math.pow (p2[1]-p1[1], 2)
++ Math.pow (p2[2]-p1[2], 2)
+);
+} // distance
+
+function randomFloat (min, max) {
+return (Math.random() * (max - min)) + min;
+} // randomFloat
+
+function randomInt (min, max) {
+return Math.floor(Math.random() * (max - min) + min) + min;
+} // randomInt
+
+} // Panner
+
+Tuna.prototype.Panner.prototype = Object.create(Tuna.Super, {
+name: {value: "Panner"},
+defaults: {
+writable: true,
+value: {
+bypass: {
+controlType: "checkbox",
+automatable: true,
+function: this.bypass,
+type: "boolean"
+}, // bypass
+
+position: {
+controlType: "text",
+value: [0,0,0],
+automatable: true,
+type: "string"
+}, // position defaults
+
+} // defaults.value
+}, // defaults
+
+// actual instance properties
+
+}); // Panner.prototype
--- tunaDemo.js
+++ tunaDemo.js
@@ -1,21 +1,38 @@
+console.log ("start...");
 var GraphicEqualizer = require ("./sources/graphicEqualizer");
 var StereoSpread = require ("./sources/stereoSpread");
+var Panner = require ("./sources/panner");
 var TunaUi = require ("./sources/tunaUi");
 var $ = require ("jquery");
+console.log ("modules loaded");
+
+
 
 var audioElement = $("#player")[0];
 var audio = new AudioContext ();
 var tuna = new Tuna (audio);
 
 var source = audio.createMediaElementSource (audioElement);
+console.log ("audio context created", audio);
+
+var panner = new tuna.Panner ();
+console.log ("Panner node created");
+var pannerUi = new TunaUi (panner, "3d Panner");
+console.log ("PannerUi created");
+pannerUi.render ( $(".panner") );
+console.log ("PannerUi rendered");
+console.log ("Panner created");
+
 
 var spread = new tuna.StereoSpread ();
 var spreadUi = new TunaUi (spread, "Stereo Spread");
 spreadUi.render ( $(".spread") );
+console.log ("spread created");
 
 var eq = new tuna.GraphicEqualizer ();
 var eqUi = new TunaUi (eq, "Equalizer");
 eqUi.render ( $(".eq") );
+console.log ("EQ created");
 
 var reverb = new tuna.Convolver({impulse: "impulse2b.wav", bypass: false});
 
@@ -26,20 +43,23 @@ reverb.defaults.level.group = 2;
 
 var reverbUi = new TunaUi (reverb, "Reverb");
 reverbUi.render ( $(".reverb") );
+console.log ("reverb created");
 
 
 spread.set ("mix", 0.4);
+spread.set ("bypass", true);
 eq.set ("Q", 1.0);
-reverb.set ("wetLevel", .4);
+reverb.set ("wetLevel", .3);
 eq.set (1024, 30);
 eq.set (2048, 30);
 eq.set ("reset");
 
 // connect graph
-source.connect (eq);
-eq.connect (reverb);
-reverb.connect (spread);
-spread.connect (audio.destination);
+source.connect (eq)
+.connect (panner)
+.connect (spread)
+.connect (reverb)
+.connect (audio.destination);
 
 // source.connect (audio.destination);
 audioElement.play ();
